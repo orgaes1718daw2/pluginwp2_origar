540ed11184f8d731e508d246a36b10ce64cb6e37
root <root@m07oriol.fjeclot.net>
2019-03-08 17:10:03 +0100

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -9,4 +9,12 @@ Author URI: https://github.com/orgaes1718daw2/
 License: GNU GPLv3
 */
 
+function admin_menu_peu_pagina(){
+$titolPagina="Peu de pàgina per defecte";
+$titolEntrada="Peu de pàgina per defecte";
+$role="administrator";
+$id="peu_pagina_per_defecte";
+add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
