d05101bba5a39e46a39d0696b6939250b2e22b59
root <root@m07oriol.fjeclot.net>
2019-03-08 16:57:00 +0100

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/orgaes1718daw2/pluginwp2_origar
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Oriol Garcia Escudero
+Author URI: https://github.com/orgaes1718daw2/
+License: GNU GPLv3
+*/
+
+?>
