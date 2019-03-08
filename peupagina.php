c02cefeaee38f062ed6226ddef8b9af407cded9c
root <root@m07oriol.fjeclot.net>
2019-03-08 17:27:25 +0100

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -10,15 +10,19 @@ License: GNU GPLv3
 */
 
 function admin_menu_peu_pagina(){
-$titolPagina="Peu de pàgina per defecte";
-$titolEntrada="Peu de pàgina per defecte";
-$role="administrator";
-$id="peu_pagina_per_defecte";
-$nom_funcio="nouPeuPagDef";
-add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
+	$titolPagina="Peu de pagina per defecte";
+	$titolEntrada="Peu de pagina per defecte";
+	$role="administrator";
+	$id="peu_pagina_per_defecte";
+	$nom_funcio="nouPeuPagDef";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio);
 }
 function nouPeuPagDef(){
-include_once('formPeuPag.php');
+	include_once('formPeuPag.php');
 }
 add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
+function afegPeuPagDef(){
+	echo "<div style='color: red; font-size: 10px; margin: 10px;'>".get_option('footer_text')."</div>";
+}
 ?>
