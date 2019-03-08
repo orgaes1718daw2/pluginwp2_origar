faaf5e45007eb2fa28e4f5e13bfad7a69b8abac9
root <root@m07oriol.fjeclot.net>
2019-03-08 17:20:41 +0100

    Commit 3

--- peupagina.php
+++ peupagina.php
@@ -14,7 +14,11 @@ $titolPagina="Peu de pàgina per defecte";
 $titolEntrada="Peu de pàgina per defecte";
 $role="administrator";
 $id="peu_pagina_per_defecte";
-add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+$nom_funcio="nouPeuPagDef";
+add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
+}
+function nouPeuPagDef(){
+include_once('formPeuPag.php');
 }
 add_action('admin_menu','admin_menu_peu_pagina');
 ?>
